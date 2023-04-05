3a85df84552083dc335f9f55f6d3644a8e86b797
Qingwen BU <64821056+retsuh-bqw@users.noreply.github.com>
2023-04-05 16:01:01 +0800

    update class-balanced random sample

--- utils/choose_index.py
+++ utils/choose_index.py
@@ -11,4 +11,14 @@ def choose_index(args, data_all_length) :
         ran_idx = random.sample(range(data_all_length),int(data_all_length*args.ratio))
     else:
         ran_idx = np.loadtxt(args.index, dtype=int)
+       
+    return ran_idx
+
+
+def choose_index_balanced(args, data_all_length, y, num_classes=10):
+    per_class_samples_num = int(data_all_length * args.ratio // num_classes)
+    idx = [np.where(y == label)[0] for label in range(num_classes)]
+    for i in range(len(idx)):
+        idx[i] = np.random.choice(idx[i], size=per_class_samples_num, replace=False)
+    ran_idx = np.concatenate(idx, axis=-1)
     return ran_idx
